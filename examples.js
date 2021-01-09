fe884ff043c1b2f70429f243045cf4c6284ac390
esarnb <esarnb@gmail.com>
2021-01-09 08:45:47 +0000

    Twitch Webhook Implementation 2020 using TwitchJS

---
+++ examples.js
@@ -0,0 +1,106 @@
+let HelixUser = {
+    _data: {
+        id: '565866981',
+        login: 'saabpar',
+        display_name: 'saabpar',
+        type: '',
+        broadcaster_type: '',
+        description: 'Retired A.V.A, CSGO and Fortnite Player (8 years of Ranked Games). COD WZ player who can hit questionable shots (check out my Medals.tv highlights) =0). I play games everyday as an outlet to relax and occasionally be toxic =0)  ',
+        profile_image_url: 'https://static-cdn.jtvnw.net/jtv_user_pictures/ca1536e0-b243-443f-b8fd-5be291aa9303-profile_image-300x300.png',
+        offline_image_url: '',
+        view_count: 354
+    }
+}
+
+let HelixUser2 = {
+    _data: {
+        id: '71092938',
+        login: 'xqcow',
+        display_name: 'xQcOW',
+        type: '',
+        broadcaster_type: 'partner',
+        description: 'Overwatch Professional tank player and full time streamer. EZ Clap',
+        profile_image_url: 'https://static-cdn.jtvnw.net/jtv_user_pictures/xqcow-profile_image-9298dca608632101-300x300.jpeg',
+        offline_image_url: 'https://static-cdn.jtvnw.net/jtv_user_pictures/d3a24a3c-85a7-4b51-96a7-d615bc70acd8-channel_offline_image-1920x1080.png',
+        view_count: 210679815
+    }
+}
+
+let HelixStream = {
+    _data: {
+        id: '40288746878',
+        user_id: '40934651',
+        user_name: 'ludwig',
+        game_id: '2010603021',
+        type: 'live',
+        title: 'SCARY GAME WITH PEOPLE',
+        viewer_count: 8741,
+        started_at: '2020-10-31T03:33:31Z',
+        language: 'en',
+        thumbnail_url: 'https://static-cdn.jtvnw.net/previews-ttv/live_user_ludwig-{width}x{height}.jpg',
+        tag_ids: [
+            '6ea6bca4-4712-4ab9-a906-e3336a9d8039',
+            '81df4005-f654-40b2-9fc0-9fd767bc8e3e',
+            '7cefbf30-4c3e-4aa7-99cd-70aabb662f27',
+            'd11b9289-806a-403b-945a-afb47ad2eea5'
+        ]
+    }
+}
+
+
+/*
+<ref *1> StreamChangeSubscription {
+  _handler: [AsyncFunction (anonymous)],
+  _client: WebHookListener {
+    _subscriptions: Map(1) { 'stream.change.77008186' => [Circular *1] },
+    _apiClient: class_1 {
+      _helixRateLimiter: [HelixRateLimiter],
+      _config: [Object],
+      cache: [Map]
+    },
+    _adapter: SimpleAdapter { _listenerPort: 3002, _hostName: 'sr.esarnb.com' },
+    _hookValidity: undefined,
+    _logger: NodeLogger {
+      _name: 'twitch-webhooks',
+      _minLevel: 2,
+      _emoji: true,
+      _colors: true,
+      _timestamps: true
+    },
+    _server: Server {
+      routes: [Array],
+      all: [Function: bound ],
+      head: [Function: bound ],
+      options: [Function: bound ],
+      get: [Function: bound ],
+      post: [Function: bound ],
+      put: [Function: bound ],
+      patch: [Function: bound ],
+      delete: [Function: bound ],
+      connect: [Function: bound ],
+      trace: [Function: bound ],
+      handleRequest: [Function (anonymous)],
+      _httpServer: [Server],
+      _onError: [Function: onError],
+      _on404: [Function: bound onError]
+    }
+  },
+  _validityInSeconds: 100000,
+  _verified: false,
+  _userId: '77008186',
+  _secret: 'X6hznfGsU1k9G721',
+  _refreshTimer: Timeout {
+    _idleTimeout: 80000000,
+    _idlePrev: [TimersList],
+    _idleNext: [TimersList],
+    _idleStart: 2300,
+    _onTimeout: [Function (anonymous)],
+    _timerArgs: undefined,
+    _repeat: 80000000,
+    _destroyed: false,
+    [Symbol(refed)]: true,
+    [Symbol(asyncId)]: 208,
+    [Symbol(triggerId)]: 0
+  }
+}
+*/
---
+++ twitch.js
@@ -0,0 +1,238 @@
+require("dotenv").config();
+require("colors");
+
+const {Client, MessageEmbed, WebhookClient} = require("discord.js");
+const { ApiClient } = require('twitch');
+const { WebHookListener, SimpleAdapter } = require('twitch-webhooks');
+const { ClientCredentialsAuthProvider } = require("twitch-auth");
+const {twitchClientID, twitchClientSecret} = process.env;
+
+const client = new Client();
+const authProvider = new ClientCredentialsAuthProvider(twitchClientID, twitchClientSecret);
+const apiClient = new ApiClient({ authProvider });
+const listener = new WebHookListener(apiClient, new SimpleAdapter({ hostName: process.env.TwitchIP, listenerPort: process.env.TwitchPort }));
+
+let axios = require("axios");
+let dayjs = require("dayjs");
+const utc = require('dayjs/plugin/utc')
+const timezone = require('dayjs/plugin/timezone')
+const relativeTime = require('dayjs/plugin/relativeTime')
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
+dayjs.extend(relativeTime)
+client.prefix = ","
+
+/* Empy */ let emp = new WebhookClient(process.env.empyHookID, process.env.empyHookToken)
+/* Saabpar */ let sbp = new WebhookClient(process.env.saabparHookID, process.env.saabparHookToken)
+/* x Test */ let test = new WebhookClient(process.env.testHookID, process.env.testHookToken);
+/* Aylin */ let aylin = new WebhookClient(process.env.aylinHookID, process.env.aylinHookToken);
+/* Venvi */ let venvi = new WebhookClient(process.env.venviHookID, process.env.venviHookToken);
+
+let streamers = {
+    "EmperorSR": { hooks: [test, emp],               subs: null, live: null }, // subs: object for subscription on/off  //live: stream object
+
+    // // Saabpar
+    "saabpar": { hooks: [sbp, venvi],           subs: null, live: null },
+    "TheresaaRere": { hooks: [sbp, aylin],      subs: null, live: null },
+    "go_malabananas": { hooks: [sbp],           subs: null, live: null },
+    "snsilentninja": { hooks: [sbp],            subs: null, live: null },
+    "malandrin861": { hooks: [sbp],             subs: null, live: null },
+    "whiskrskittles7": { hooks: [sbp],          subs: null, live: null },
+    "yngplo": { hooks: [sbp],                   subs: null, live: null },
+
+    // // Aylin
+    "bonedipcollect": { hooks: [sbp, aylin],    subs: null, live: null },
+    "brotherpiko": { hooks: [aylin],            subs: null, live: null },
+    "mr_shorty13": { hooks: [aylin],            subs: null, live: null },
+    "lineant": { hooks: [aylin],                subs: null, live: null },
+    "trianglemikey": { hooks: [aylin],          subs: null, live: null },
+    "nyxnxn": { hooks: [aylin],                 subs: null, live: null },
+    "CHANCEBEFLYAf": { hooks: [aylin],          subs: null, live: null },
+
+    // // Personal
+    // "xQcOW": { hooks: [emp],                 subs: null, live: null },
+    // "Souljaboy": { hooks: [emp],             subs: null, live: null },
+    "michaelreeves": { hooks: [emp],            subs: null, live: null },
+    // "Smii7y": { hooks: [emp],                subs: null, live: null },
+
+}
+
+client.login(process.env.TOKEN)
+
+client.on("ready", async () => {
+    console.log(`[Twitch] ${client.user.tag} is now online!`.green);
+    await listener.listen();
+    console.log(`Twitch client online on port ${process.env.TwitchPort}, Loading Streamers. . .`.green);
+    
+    for (let keyName in streamers) {
+        let user = await apiClient.helix.users.getUserByName(keyName)
+        if (user) {
+            let {displayName, id, offlinePlaceholderUrl, profilePictureUrl} = user;
+            let userid = id;
+
+            console.log(`Loading Streamer ${displayName}...`.yellow);
+
+            let prevStream = await apiClient.helix.streams.getStreamByUserName(displayName); 
+            if (prevStream) streamers[keyName].live = prevStream; // According to bot, it sees there is already a stream in progress.
+
+            const subscription = await listener.subscribeToStreamChanges(userid, async (stream) => {
+                if (stream) {
+                    streamers[keyName].live = stream; // update local storage of user's stream data
+                    let { gameId, id, startDate, thumbnailUrl, title, type, userDisplayName} = stream;
+                    let streamId = id, embed = new MessageEmbed();
+
+                    // User just started the stream.
+                    if (!prevStream) {
+                        embed.setTitle(`${userDisplayName} is streaming!`);
+                        embed.addField(title, `Started Streaming: ${dayjs(startDate).tz("America/Los_Angeles").format('MM/DD/YYYY hh:mm:ssa')} PST`);
+                    } 
+                    
+                    // User has been streaming.
+                    else {
+                        embed.fields.length = 0; // Erase previous titles if there are any.
+                        embed.addField(title, `Updated Stream: ${dayjs().tz("America/Los_Angeles").format('MM/DD/YYYY hh:mm:ssa')} PST`);
+                    }
+
+                    // Fetch the image provided by twitch's livestream URL property with axios (bypassing Discord's Cache Problem)
+                    let fetchLive = await axios.get(thumbnailUrl.replace("-{width}x{height}", ""), {responseType: 'arraybuffer'})
+                    embed.attachFiles([{name: "livestream.png", attachment: fetchLive.data}]).setImage('attachment://livestream.png')
+                    
+                    // Fetch Game Data for the current moment
+                    let game = await stream.getGame();
+                    if (game) {
+                        let newBoxStr = game.boxArtUrl.replace("-{width}x{height}", "")
+                        let fetchGamePic = await axios.get(newBoxStr.replace("-{width}x{height}", ""), {responseType: 'arraybuffer'})
+                        embed.attachFiles([{name: "game.png", attachment: fetchGamePic.data}]).setThumbnail('attachment://game.png')
+                        // embed.setThumbnail(newBoxStr)
+                    }
+                    
+                    // embed.setFooter(`userid: ${userid} | streamid: ${streamId} | gameid: ${gameId} | type: ${type}`, profilePictureUrl)
+                    embed.setDescription(`Stream Link: [https://www.twitch.tv/${displayName}](https://www.twitch.tv/${displayName})`);
+                  
+                    streamers[keyName].hooks.every(hook => hook.send({embeds: [embed]})) 
+                
+                } 
+                
+                else {
+                    embed = new MessageEmbed();
+                    if (offlinePlaceholderUrl) embed.setImage(offlinePlaceholderUrl)
+                    if (streamers[keyName].live) {
+                        let durationSince = dayjs().from(streamers[keyName].live.startDate, true)
+                        embed.addField(`Thank you for coming!`, `Streamed for ${durationSince}`)
+                    }
+                    
+                    // no stream, User is now offline.
+                    embed.setThumbnail(profilePictureUrl)
+                    embed.setTitle(`${displayName} is now offline.`)
+                    embed.setFooter(`Time: ${dayjs().tz("America/Los_Angeles").format('MM/DD/YYYY hh:mm:ssa')} PST`, profilePictureUrl)
+
+                    // streamers[keyName].streamers[keyName].live = null;
+                    streamers[keyName].live = null;
+
+                    streamers[keyName].hooks.every(hook => hook.send({embeds: [embed]})) 
+
+                }
+
+            });
+
+            subscription.start().then(() => {
+                // streamers[keyName].sub = subscription;
+                streamers[keyName].subs = subscription;
+                console.log(`Hooked ${displayName}:${id}`.grey);
+            })
+        }
+    }
+    console.log("Done!".cyan)
+})
+
+client.on("message", (msg) => {
+    if (msg.author.bot || (msg.content.startsWith(client.prefix) && msg.content.charAt(msg.content.length - 1) == client.prefix)) return;
+
+    //Setup prefix
+    let prefixes = [client.prefix, `<@${client.user.id}> `, `<@!${client.user.id}> `];
+    for (thisPrefix of prefixes) if (msg.content.startsWith(thisPrefix)) client.prefix = thisPrefix;
+    if(msg.content.indexOf(client.prefix) !== 0) return;
+    
+    //Load Args, cmd
+    const args = msg.content.slice(client.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    switch (command) {
+        case "ping": msg.channel.send("Pong c:"); break;
+        case "streaming": streaming_cmd(msg, args); break;
+        case "twitch": twitch_cmd(msg, args); break;
+        case "help": help_cmd(msg, args); break;
+        default: break;
+    }
+
+    return;
+})
+
+
+function streaming_cmd(m, args) {
+    let embed = new MessageEmbed();
+    let online = [];
+    for (x in streamers) if (streamers[x].live) online.push(streamers[x].live);
+    online = online.map((x,i) => `${i+1}. [[${x.userDisplayName} is streaming!](https://www.twitch.tv/${x.userDisplayName})] ${x.title}\n`)
+    embed.setDescription(online.join("\n"))
+    m.channel.send({ embed: embed })
+}
+
+
+function help_cmd(m, args) {
+    let embed = new MessageEmbed();
+    embed.setTitle("Commands List")
+    .addField("twitch <username>", `
+        > See the current status of a twitch user
+        Example: \`${client.prefix}twitch TheresaaRere\`
+    `)
+    .addField("streaming", `
+        > See the current users streaming for the specific guild. Guild streamers list are shown in the bottom of the embed.
+        Example: \`${client.prefix}streaming\`
+    `)
+    m.channel.send({embed: embed})
+}
+
+async function twitch_cmd(m, args) {
+    let embed = new MessageEmbed();
+    if (!args[0]) return m.channel.send("Format: `>twitch saabpar`");
+    
+    // User Setup //
+    let user = await apiClient.helix.users.getUserByName(args[0]);
+    if (!user) return m.channel.send("User does not exist.");
+    let { profilePictureUrl, displayName, description } = user;
+
+    // Stream Setup //
+    let prevStream = await apiClient.helix.streams.getStreamByUserName(displayName);
+    if (!prevStream) embed.setTitle(displayName + " is offline.").addField("Profile link", `[https://www.twitch.tv/${displayName}](https://www.twitch.tv/${displayName})`)
+    else {
+        let { userDisplayName, title, viewers, startDate, type, thumbnailUrl } = prevStream;
+        let game = await prevStream.getGame();
+        
+        if (thumbnailUrl) {
+            // Fetch the image provided by twitch's livestream URL property with axios (bypassing Discord's Cache Problem)
+            let res = await axios.get(thumbnailUrl.replace("-{width}x{height}", ""), {responseType: 'arraybuffer'})
+            embed.attachFiles([{name: "livestream.png", attachment: res.data}]).setImage('attachment://livestream.png')
+        }
+
+        if (game) {
+            let {name, boxArtUrl} = game;
+            if (!thumbnailUrl) embed.setImage(`${boxArtUrl.slice(0, boxArtUrl.length - 21)}.jpg`)
+            embed.addField("Game", name)
+        } 
+
+        embed.setTitle(`${userDisplayName} is live!`)
+            .setDescription(`${title}\n[ View the stream here! ](https://www.twitch.tv/${userDisplayName})`)
+            .setFooter(`Livestream Viewer Count: ${viewers} | Started Streaming: ${dayjs(startDate).tz("America/Los_Angeles").format('MM/DD/YYYY hh:mm:ssa')} PST`)
+    }
+
+    embed.setThumbnail(profilePictureUrl).addField("User Profile", `Name: ${displayName}\n‏‏‎Description: ${description}`)
+    m.channel.send({ embed: embed })
+}
+
+process.on("SIGINT", async () => {
+    // Kill all twitch subscription listeners beforing ending script. 
+    for (x in streamers) streamers[x].subs ? streamers[x].subs.stop() : null;
+    process.exit()
+})
